dbc98484af519fd95217495444e4bf896b41b642
Rusei Marius <86234350+eosgmr@users.noreply.github.com>
2024-04-10 11:07:06 +0300

    Update ContentModal.js

--- moviewatch/src/components/contentModal/ContentModal.js
+++ moviewatch/src/components/contentModal/ContentModal.js
@@ -11,7 +11,7 @@ const style = {
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: "60%",
-  height:"70%",
+  height: "70%",
   bgcolor: 'black',
   borderRadius: 5,
   color: "white",
@@ -20,7 +20,7 @@ const style = {
   p: 4,
 };
 
-export default function ContentModal({children, title, poster, vote, overview}) {
+export default function ContentModal({ children, title, poster, vote, overview }) {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -36,18 +36,18 @@ export default function ContentModal({children, title, poster, vote, overview}) 
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-        <Button onClick={handleClose} style={{ position: 'absolute', top: 10, right: 10 }}>Exit</Button>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          {<img className="poster" src={poster ? `${img_300}/${poster} ` : unavailable} alt={title} />} 
+          <Button onClick={handleClose} style={{ position: 'absolute', top: 10, right: 10 }}>Exit</Button>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            {<img className="posterbox" src={poster ? `${img_300}/${poster} ` : unavailable} alt={title} />}
+          </Typography>
+          <Typography id="modal-modal-title" variant="h6" component="h2" style={{ maxHeight: '60px', overflowY: 'auto' }}>
+            {title}
           </Typography>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-          {title} 
+            <span className='vote'>⭐{vote}</span>
           </Typography>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-          <span className='vote'>⭐{vote}</span>
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-           {overview}
+          <Typography id="modal-modal-description" sx={{ mt: 2 }} style={{ maxHeight: '200px', overflowY: 'auto' }}>
+            {overview}
           </Typography>
         </Box>
       </Modal>
